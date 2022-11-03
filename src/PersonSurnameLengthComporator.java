0880fb7b3e88bf1388a45866cfaebd3c03c2f2d8
Albert Sayapov <asayapov@mail.ru>
2022-11-03 14:07:01 +0700

    Add person1 person2

--- src/PersonSurnameLengthComporator.java
+++ src/PersonSurnameLengthComporator.java
@@ -10,14 +10,17 @@ public class PersonSurnameLengthComporator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getSurname().length() > size && o2.getSurname().length() > size) {
+        int person1 = o1.getSurname().length();
+        int person2 = o2.getSurname().length();
+
+        if (person1 > size && person2 > size) {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
 
-        if (o1.getSurname().length() < o2.getSurname().length()) {
-            return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
-        } else if (o1.getSurname().length() > o2.getSurname().length()) {
-            return Integer.compare(o1.getSurname().length(), o2.getSurname().length());
+        if (person1 < person2) {
+            return Integer.compare(person1, person2);
+        } else if (person1 > person2) {
+            return Integer.compare(person1, person2);
         } else {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
