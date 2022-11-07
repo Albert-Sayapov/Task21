a7d8a32029ffb5dcd8cddbd0ec6749248dba46ff
Albert Sayapov <asayapov@mail.ru>
2022-11-07 20:19:07 +0700

    add removeIf(predicate)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
 
         personList.add(new Person("Альберт", "123456", 10));
-        personList.add(new Person("Андрей", "123", 15));
+        personList.add(new Person("Андрей", "123", 20));
         personList.add(new Person("Алексей", "1234", 14));
         personList.add(new Person("Роман", "1234", 18));
+
+        int maxAge = 18;
+
+        Predicate<Person> predicate = person -> person.getAge() < maxAge;
+
+        personList.removeIf(predicate);
 
         Collections.sort(personList, new PersonSurnameLengthComporator(3));
         System.out.println(personList);
